1c2680491e9bb8e84bcda4330571f7b261d49d88
BamanGirma <amanuelgirma360@gmail.com>
2024-11-01 11:58:25 +0300

    update database connection

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -5,7 +5,7 @@ dotenv.config()
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
 
